de935f1508714d6349e9a62b800f7e8c867f20a7
George Vince <george.vince@hotmail.co.uk#>
2017-07-25 12:32:25 +0100

    Removed header comment

--- src/final_data.py
+++ src/final_data.py
@@ -2,63 +2,6 @@
 # -*- coding: utf-8 -*-
 
 """
-After auditing is complete the next step is to prepare the data to be inserted into a SQL database.
-To do so you will parse the elements in the OSM XML file, transforming them from document format to
-tabular format, thus making it possible to write to .csv files.  These csv files can then easily be
-imported to a SQL database as tables.
-
-The process for this transformation is as follows:
-- Use iterparse to iteratively step through each top level element in the XML
-- Shape each element into several data structures using a custom function
-- Utilize a schema and validation library to ensure the transformed data is in the correct format
-- Write each data structure to the appropriate .csv files
-
-We've already provided the code needed to load the data, perform iterative parsing and write the
-output to csv files. Your task is to complete the shape_element function that will transform each
-element into the correct format. To make this process easier we've already defined a schema (see
-the schema.py file in the last code tab) for the .csv files and the eventual tables. Using the 
-cerberus library we can validate the output against this schema to ensure it is correct.
-
-## Shape Element Function
-The function should take as input an iterparse Element object and return a dictionary.
-
-### If the element top level tag is "node":
-The dictionary returned should have the format {"node": .., "node_tags": ...}
-
-The "node" field should hold a dictionary of the following top level node attributes:
-- id
-- user
-- uid
-- version
-- lat
-- lon
-- timestamp
-- changeset
-All other attributes can be ignored
-
-The "node_tags" field should hold a list of dictionaries, one per secondary tag. Secondary tags are
-child tags of node which have the tag name/type: "tag". Each dictionary should have the following
-fields from the secondary tag attributes:
-- id: the top level node id attribute value
-- key: the full tag "k" attribute value if no colon is present or the characters after the colon if one is.
-- value: the tag "v" attribute value
-- type: either the characters before the colon in the tag "k" value or "regular" if a colon
-        is not present.
-
-Additionally,
-
-- if the tag "k" value contains problematic characters, the tag should be ignored
-- if the tag "k" value contains a ":" the characters before the ":" should be set as the tag type
-  and characters after the ":" should be set as the tag key
-- if there are additional ":" in the "k" value they and they should be ignored and kept as part of
-  the tag key. For example:
-
-  <tag k="addr:street:name" v="Lincoln"/>
-  should be turned into
-  {'id': 12345, 'key': 'street:name', 'value': 'Lincoln', 'type': 'addr'}
-
-- If a node has no secondary tags then the "node_tags" field should just contain an empty list.
-
 The final return value for a "node" element should look something like:
 
 {'node': {'id': 757860928,
@@ -82,28 +25,7 @@ The final return value for a "node" element should look something like:
                 'value': "Shelly's Tasty Freeze",
                 'type': 'regular'}]}
 
-### If the element top level tag is "way":
-The dictionary should have the format {"way": ..., "way_tags": ..., "way_nodes": ...}
 
-The "way" field should hold a dictionary of the following top level way attributes:
-- id
--  user
-- uid
-- version
-- timestamp
-- changeset
-
-All other attributes can be ignored
-
-The "way_tags" field should again hold a list of dictionaries, following the exact same rules as
-for "node_tags".
-
-Additionally, the dictionary should have a field "way_nodes". "way_nodes" should hold a list of
-dictionaries, one for each nd child tag.  Each dictionary should have the fields:
-- id: the top level element (way) id
-- node_id: the ref attribute value of the nd tag
-- position: the index starting at 0 of the nd tag i.e. what order the nd tag appears within
-            the way element
 
 The final return value for a "way" element should look something like:
 
@@ -132,22 +54,6 @@ The final return value for a "way" element should look something like:
                'key': 'street:name',
                'type': 'addr',
                'value': 'Lexington'},
-              {'id': '209809850',
-               'key': 'street:prefix',
-               'type': 'addr',
-               'value': 'West'},
-              {'id': 209809850,
-               'key': 'street:type',
-               'type': 'addr',
-               'value': 'Street'},
-              {'id': 209809850,
-               'key': 'building',
-               'type': 'regular',
-               'value': 'yes'},
-              {'id': 209809850,
-               'key': 'levels',
-               'type': 'building',
-               'value': '1'},
               {'id': 209809850,
                'key': 'building_id',
                'type': 'chicago',
@@ -294,7 +200,8 @@ def format_street(street_name):
     
     if street_altered <> street_name:
         print "Street Modified - {} -> {}".format(street_name, street_altered)
-            
+
+           
 def update_street_name(name, mapping):
     
     replace = name.rsplit(None, 1)[-1]
